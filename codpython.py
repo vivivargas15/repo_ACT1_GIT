b8c504c834a4bb68cfec40ded34c21c2dfbb6e18
vivivargas <viviana_vargas15@hotmail.com>
2022-10-11 20:52:10 -0500

    Agregamos codigo de HOMERO del nivel 1

---
+++ codpython.py
@@ -0,0 +1,12 @@
+inicio= input("Para iniciar presione cualquier tecla\n")
+temperatura = input (" Desea comprobar la temperatura central - si / no \n")
+if temperatura == "si":
+	print("la temperatura central es normal  \n")
+else:
+	print("Recuerde comprobar la temperatura mas adelante \n")
+
+gas= input("Desea ventilar gas radioactivo?- si / no \n")
+if gas == "no":
+	print ( " Ventilar previene EXPLOSIONES ")
+else: 
+	print("BOOOOMMMMMM!!!!!")
